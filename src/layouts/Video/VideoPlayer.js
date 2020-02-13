e80776c8ab6e1e055eb69041804eea5d452ac1ea
ag502 <ag502@naver.com>
2020-02-13 23:43:08 +0900

    Add trimming slide

--- src/layouts/Video/VideoPlayer.js
+++ src/layouts/Video/VideoPlayer.js
@@ -8,7 +8,9 @@ import MuiDialogContent from '@material-ui/core/DialogContent';
 import MuiDialogActions from '@material-ui/core/DialogActions';
 import Slider from '@material-ui/core/Slider';
 import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
+import Paper from '@material-ui/core/Paper';
+import Fade from '@material-ui/core/Fade';
+import {Close, PlayArrow, Pause, VolumeDown, VolumeMute, VolumeOff, VolumeUp} from '@material-ui/icons';
 import Typography from '@material-ui/core/Typography';
 import ReactPlayer from 'react-player';
 import {VideoSeekSlider} from 'react-video-seek-slider';
@@ -35,7 +37,7 @@ const DialogTitle = withStyles(styles)(props => {
       <Typography variant="h6">{children}</Typography>
       {onClose ? (
         <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
-          <CloseIcon />
+          <Close />
         </IconButton>
       ) : null}
     </MuiDialogTitle>
@@ -56,22 +58,20 @@ const DialogActions = withStyles(theme => ({
 }))(MuiDialogActions);
 
 const useStyles = makeStyles(() => ({
-  playerContainer: {
-    backgroundColor: 'gray',
-    width: '640px'
+  sliderContainer: {
+    width: '640px',
+    padding: '0 7px 0 7px'
   },
   slider: {
-    width: '640px',
+    width: '100%',
   }
 }));
 
-function VideoPlay({videoID}) {
-  const [isPlay, setPlay] = useState(false);
-  const [volume, setVolume] = useState(0.7);
-  const [isMute, setMute] = useState(false);
+function VideoPlay({
+                     videoID,
+                     getTrimmingPoint,
+                   }) {
   const [duration, setDuration] = useState(0);
-  const [progress, setProgress] = useState({ playedSeconds: 0, played: 0, loadedSeconds: 0, loaded: 0 });
-  const [isSeeking, setSeeking] = useState(false);
   const playerRef = useRef(null);
 
   const classes = useStyles();
@@ -81,56 +81,35 @@ function VideoPlay({videoID}) {
     setDuration(duration);
   };
 
-  const handleProgress = progressState => {
-    if (!isSeeking) {
-      setProgress(progressState);
-    }
-  };
-
   return (
     <>
       <ReactPlayer
         url={`https://www.youtube.com/watch?v=${videoID}`}
         ref={playerRef}
         controls={true}
-        playing
-        onPlay={() => console.log('hi')}
         onDuration={handleDuration}
-        onProgress={handleProgress}
         onSeek={(second) => console.log(second)}
       />
-      <div className={classes.playerContainer}>
-        <TrimSlider duration={duration}/>
-        <VideoSeekSlider
-          max={duration}
-          progress={400}
-          currentTime={progress.playedSeconds}
-          onChange={(time) => {
-            setSeeking(true);
-            playerRef.current.seekTo(time);
-            setProgress((state) => {
-              return {
-                ...state,
-                playedSeconds: time
-              }
-            });
-            setSeeking(false);
-          }}
-          offset={0}
-          secondsPrefix="00:00:"
-          minutesPrefix="00:"
+      <div className={classes.sliderContainer}>
+        <TrimSlider
+          duration={duration}
+          getTrimmingPoint={getTrimmingPoint}
         />
       </div>
     </>
   )
 }
 
-function TrimSlider({duration}) {
+function TrimSlider({
+                      duration,
+                      getTrimmingPoint
+                    }) {
   const classes = useStyles();
   const [value, setValue] = React.useState([0, 0]);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
+    getTrimmingPoint(value);
   };
 
   const secondToMinute = value => {
@@ -151,7 +130,7 @@ function TrimSlider({duration}) {
   };
 
   return (
-    <div className={classes.root}>
+    <div className={classes.slider}>
       <Slider
         value={value}
         max={duration - 1}
@@ -171,9 +150,21 @@ function VideoPopWindow({
                         }) {
 
   const dispatch = useDispatch();
+  let trimmingPoint = [0, 0];
 
   const handleClose = () => {
     dispatch(closeVideo());
+  };
+
+  const handleClipping = event => {
+    console.log('Trimming');
+    // 클리핑 요청 보낼 부분
+    // trimmingPoint는 cliping할 처음과 끝 부분, slider를 움직이지 않으면 0, 0을 가르킴
+    // axios.post("#");
+  };
+
+  const getTrimmingPoint = (value) => {
+    trimmingPoint = value;
   };
 
   return (
@@ -182,18 +173,20 @@ function VideoPopWindow({
         onClose={handleClose}
         aria-labelledby="customized-dialog-title"
         maxWidth={'md'}
-        fullWidth={true}
+        fullWidth={false}
         open={isPlay}
       >
         <DialogTitle id="customized-dialog-title" onClose={handleClose}>
           {title}
         </DialogTitle>
         <DialogContent dividers>
-          <VideoPlay videoID={videoID}/>
+          <VideoPlay
+            videoID={videoID}
+            getTrimmingPoint={getTrimmingPoint}/>
         </DialogContent>
         <DialogActions>
-          <Button autoFocus onClick={handleClose} color="primary">
-            Save changes
+          <Button autoFocus onClick={handleClipping} color="primary">
+            Clipping
           </Button>
         </DialogActions>
       </Dialog>
