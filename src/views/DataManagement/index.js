47704ec87a7f367287f110b9290e27b33d643826
ag502 <ag502@naver.com>
2020-02-16 23:45:23 +0900

    Add Template of DataProcess page and DataManagement page
    Connect each pages at router

---
+++ src/views/DataManagement/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Page from 'src/components/Page';
+
+function DataManagement() {
+  return (
+    <Page title="DataManagement">
+      <div>This is Data ManageMent Page</div>
+    </Page>
+  );
+}
+
+export default DataManagement;
---
+++ src/views/DataProcess/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Page from 'src/components/Page';
+
+function DataProcess() {
+  return (
+    <Page title="DataProcess">
+      <div>This is Data Process Page</div>
+    </Page>
+  );
+}
+
+export default DataProcess;
