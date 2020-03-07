8d18ee95bce97d3ed16caa28996aef72d563cb37
JaeHeee <kjha2142@naver.com>
2020-03-07 23:39:42 +0900

    Delete storages.py

--- backend/config/storages.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from storages.backends.s3boto3 import S3Boto3Storage
-
-
-class ClippingVideoStorage(S3Boto3Storage):
-    location = settings.CLIPPINGVIDEO_LOCATION
-
-class ThumnailStorage(S3Boto3Storage):
-    location = settings.THUMNAIL_LOCATION
